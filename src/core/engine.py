acb5c9190f516949f52b3bd84f62cf771f9dc050
tayadj <tayadj@users.noreply.github.com>
2025-04-18 16:32:09 +0300

    Database enhancement, knowledge generation minor implementations

--- src/core/engine.py
+++ src/core/engine.py
@@ -1,6 +1,7 @@
 import llama_index
 import llama_index.llms.openai
 import os
+import pandas
 
 from .services import OntologyProcessor, NexusProcessor
 
@@ -15,11 +16,11 @@ class Engine:
 		self.ontology_processor = OntologyProcessor(self.model)
 		self.nexus_processor = NexusProcessor()
 
-	def generate_ontology(self, dataframe):
+	def generate_ontology(self, documents_dataframe):
 
 		ontology = []
 
-		for idx, record in dataframe.iterrows():
+		for idx, record in documents_dataframe.iterrows():
 
 			result = self.ontology_processor.process(record.text)
 			ontology.append(
@@ -33,3 +34,17 @@ class Engine:
 			)
 
 		return ontology
+
+	def generate_knowledge(self, documents_dataframe, ontology_dataframe):
+
+		knowledge_dataframe = pandas.merge(ontology_dataframe, documents_dataframe, left_on = 'chunk_source', right_on = 'file_name', how = 'inner')
+
+		knowledge_documents = []
+
+		for document_text in knowledge_dataframe['text'].unique():
+
+			knowledge_documents.append(llama_index.core.schema.Document(text = document_text))
+
+		print(knowledge_documents)
+
+		
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import utils
 if __name__ == '__main__':
 
 	settings = config.Settings()
-	database = data.Database({'LOCAL_STORAGE_URL': settings.LOCAL_STORAGE_URL.get_secret_value()})
+	database = data.Database(local_storage_url = settings.LOCAL_STORAGE_URL.get_secret_value())
 	engine = core.Engine(settings.OPENAI_API_KEY.get_secret_value())
 
 	'''
@@ -23,21 +23,14 @@ if __name__ == '__main__':
 	ontology_dataframe.to_csv(settings.LOCAL_STORAGE_URL.get_secret_value() + '_ontology.csv', index = False)
 	'''
 
+
+	# Knowldege pipeline
 	documents = database.local_connector.load()
 	documents_dataframe = utils.converters.documentsToDataframe(documents)
 
 	ontology_dataframe = pandas.read_csv(settings.LOCAL_STORAGE_URL.get_secret_value() + '_ontology.csv')
 
-	knowledge_dataframe = pandas.merge(ontology_dataframe, documents_dataframe, left_on = 'chunk_source', right_on = 'file_name', how = 'inner')
-
-	knowledge_documents = []
-
-	for document_text in knowledge_dataframe['text'].unique():
-
-		knowledge_documents.append(llama_index.schema.Document(document_text))
-
-
-
+	engine.generate_knowledge(documents_dataframe, ontology_dataframe)
 
 	# utils.visualization.ontologyVisualization(settings, ontology_dataframe)
 	
