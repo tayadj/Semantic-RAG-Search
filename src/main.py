3dac952a860b35d7b934665ae3433f7bb96b4fc3
tayadj <tayadj@users.noreply.github.com>
2025-04-19 22:48:05 +0300

    Differentiated pipelines

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import llama_index
 import pandas
 
 import config
@@ -7,24 +8,19 @@ import utils
 
 
 
-if __name__ == '__main__':
+def ontology_pipeline(settings, database, engine):
 
-	settings = config.Settings()
-	database = data.Database(local_storage_url = settings.LOCAL_STORAGE_URL.get_secret_value())
-	engine = core.Engine(settings.OPENAI_API_KEY.get_secret_value())
-
-	'''
-	# Ontology pipeline
 	documents = database.local_connector.load()
 	documents_dataframe = utils.converters.documentsToDataframe(documents)
 
 	ontology = engine.generate_ontology(documents_dataframe)
 	ontology_dataframe = utils.converters.ontologyToDataframe(ontology)
 	ontology_dataframe.to_csv(settings.LOCAL_STORAGE_URL.get_secret_value() + '_ontology.csv', index = False)
-	'''
 
+	utils.visualization.ontologyVisualization(settings, ontology_dataframe)
 
-	# Knowldege pipeline
+def knowledge_pipeline(settings, database, engine):
+
 	documents = database.local_connector.load()
 	documents_dataframe = utils.converters.documentsToDataframe(documents)
 
@@ -32,6 +28,25 @@ if __name__ == '__main__':
 
 	knowledge_index = engine.generate_knowledge(documents_dataframe, ontology_dataframe)
 
-	# utils.visualization.ontologyVisualization(settings, ontology_dataframe)
-	
+def inference_pipeline(settings, database, engine)
+
+	documents = database.local_connector.load()
+	documents_dataframe = utils.converters.documentsToDataframe(documents)
+
+	ontology_dataframe = pandas.read_csv(settings.LOCAL_STORAGE_URL.get_secret_value() + '_ontology.csv')
+
+	knowledge_index = engine.generate_knowledge(documents_dataframe, ontology_dataframe)
+
+	query_engine = knowledge_index.as_query_engine(include_text = True, response_mode = 'tree_summarize')
+	query_text = "Tell me more about American society and its influence on the cultural landscape."
+	response = query_engine.query(query_text)
+	print("LLM Response:\n", response)
+
+
+
+if __name__ == '__main__':
+
+	settings = config.Settings()
+	database = data.Database(local_storage_url = settings.LOCAL_STORAGE_URL.get_secret_value())
+	engine = core.Engine(settings.OPENAI_API_KEY.get_secret_value())
 	
